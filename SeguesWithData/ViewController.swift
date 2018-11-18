bf0e69c49196748bff899d34e92a002f34cb6340
vikash tank <vikashtank@hotmail.co.uk>
2018-11-18 21:07:32 +0000

    Initial Commit

---
+++ SeguesWithData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeguesWithData
+//
+//  Created by VIkash Tank on 18/11/2018.
+//  Copyright © 2018 VIkash Tank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
